4ba337c55641e729d0b86d7a6c78d38091c44593
Crowdbotics <team@crowdbotics.com>
2023-11-10 17:28:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lawrence-west-app-44469",
+  url: "https://lawrence-west-app-44469.botics.co",
+  api: "https://lawrence-west-app-44469.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
