431239ca57deb075a1da74edcc428458fd998bcc
Crowdbotics <team@crowdbotics.com>
2023-12-04 21:20:47 +0000

    add component User Profile

--- modules/user-profile/styles.js
+++ modules/user-profile/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+
 export const Color = {
   malibu: "#46E1FD",
   pink: "#e97d77",
@@ -10,6 +11,7 @@ export const Color = {
   red: "red",
   gray: "#6A6A6A"
 };
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -33,43 +35,27 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 5
   },
-  label: {
-    color: Color.gray,
-    fontSize: 12,
-    fontWeight: "bold"
-  },
-  text: {
-    color: Color.black,
-    fontSize: 16,
-    marginTop: 3,
-    marginBottom: 12
-  }
+  label: { color: Color.gray, fontSize: 12, fontWeight: "bold" },
+  text: { color: Color.black, fontSize: 16, marginTop: 3, marginBottom: 12 }
 });
+
 export const inputStyles = {
   input: {
     borderColor: Color.steel,
     borderWidth: 0.9,
     borderRadius: 6,
     fontSize: 15,
+
     marginVertical: 10,
     paddingHorizontal: 15,
     paddingVertical: 7,
     color: Color.black
   },
-  date: {
-    width: "100%",
-    paddingVertical: 0
-  },
-  label: {
-    color: Color.gray,
-    fontSize: 12,
-    fontWeight: "bold"
-  },
-  error: {
-    color: Color.red,
-    fontSize: 9
-  }
+  date: { width: "100%", paddingVertical: 0 },
+  label: { color: Color.gray, fontSize: 12, fontWeight: "bold" },
+  error: { color: Color.red, fontSize: 9 }
 };
+
 export const buttonStyles = {
   view: {
     backgroundColor: Color.pink,
