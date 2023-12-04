e4b782cc8b3ebbde32e49a69105eed74e7987ce5
Crowdbotics <team@crowdbotics.com>
2023-12-04 21:33:47 +0000

    add component Terms and Conditions

--- modules/terms-and-conditions/styles.js
+++ modules/terms-and-conditions/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+
 export const styles = StyleSheet.create({
   heading: {
     height: 60,
@@ -13,19 +14,8 @@ export const styles = StyleSheet.create({
     fontSize: 16,
     width: "100%"
   },
-  icon: {
-    width: 18,
-    height: 16
-  },
-  touchableopacity: {
-    padding: 5
-  },
-  scrollview: {
-    flex: 1,
-    padding: 20
-  },
-  header: {
-    color: "#fff",
-    fontSize: 16
-  }
+  icon: { width: 18, height: 16 },
+  touchableopacity: { padding: 5 },
+  scrollview: { flex: 1, padding: 20 },
+  header: { color: "#fff", fontSize: 16 }
 });
