7666da40644ff0d81ef35396e6fc3f92d1211bdf
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-12-04 23:18:58 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/faq/index.js
+++ modules/faq/index.js
@@ -1,14 +1,8 @@
+import { StyleSheet } from "react-native";
 import { GlobalOptionsContext, OptionsContext } from "@options";
 import { useNavigation } from "@react-navigation/native";
 import React, { useContext, useEffect, useState } from "react";
-import {
-  FlatList,
-  Image,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
-} from "react-native";
+import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { Divider } from "react-native-elements";
 import { Images } from "./assets";
@@ -21,42 +15,29 @@ const FAQ = () => {
   const navigation = useNavigation();
   const [searchText, setSearchText] = useState("");
   const [pageNo, setPageNo] = useState(1);
-  const faqData = useSelector((state) => state.Faq);
-  const { faq } = faqData;
+  const faqData = useSelector(state => state.Faq);
+  const {
+    faq
+  } = faqData;
   const list = faq?.results ?? [];
   const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(faqList({ baseUrl: globalOptions.url, page: pageNo }));
+    dispatch(faqList({
+      baseUrl: globalOptions.url,
+      page: pageNo
+    }));
   }, [pageNo, faqList]);
+  const filterList = list.filter(d => d.question.toLowerCase().includes(searchText.toLowerCase()) || d.answer.toLowerCase().includes(searchText.toLowerCase()));
 
-  const filterList = list.filter(
-    (d) =>
-      d.question.toLowerCase().includes(searchText.toLowerCase()) ||
-      d.answer.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-  const searchHandler = (text) => {
+  const searchHandler = text => {
     setSearchText(text);
   };
 
   const searchView = () => {
-    return (
-      <View style={options.styles.searchSection}>
-        <Image
-          style={options.styles.searchIcon}
-          source={Images.searchIcon}
-          resizeMode="contain"
-        />
-        <TextInput
-          style={options.styles.input}
-          placeholder="Search here"
-          onChangeText={searchHandler}
-          placeholderTextColor={options.colors.darkGray}
-          value={searchText}
-        />
-      </View>
-    );
+    return <View style={options.styles.searchSection}>
+        <Image style={options.styles.searchIcon} source={Images.searchIcon} resizeMode="contain" />
+        <TextInput style={options.styles.input} placeholder="Search here" onChangeText={searchHandler} placeholderTextColor={options.colors.darkGray} value={searchText} />
+      </View>;
   };
 
   const fetchMoreData = () => {
@@ -65,48 +46,25 @@ const FAQ = () => {
     }
   };
 
-  const _renderItem = ({ item }) => {
-    return (
-      <FAQItem
-        {...item}
-        prefixQuestion={faq?.prefix_question}
-        prefixAnswer={faq?.prefix_answer}
-      />
-    );
+  const _renderItem = ({
+    item
+  }) => {
+    return <FAQItem {...item} prefixQuestion={faq?.prefix_question} prefixAnswer={faq?.prefix_answer} />;
   };
 
   const separator = () => {
     return <Divider color={options.colors.darkCharcoal} />;
   };
 
-  return (
-    <View style={options.styles.container}>
+  return <View style={options.styles.container}>
       <View style={options.styles.heading}>
-        <TouchableOpacity
-          style={{ padding: 5 }}
-          onPress={() => navigation.goBack()}
-        >
-          <Image
-            style={options.styles.backIcon}
-            source={Images.backIcon}
-            resizeMode="contain"
-          />
+        <TouchableOpacity style={_styles.HaVZuDPd} onPress={() => navigation.goBack()}>
+          <Image style={options.styles.backIcon} source={Images.backIcon} resizeMode="contain" />
         </TouchableOpacity>
         <Text style={options.styles.title}>{options.title}</Text>
       </View>
-      <FlatList
-        data={filterList}
-        renderItem={_renderItem}
-        keyExtractor={(item) => item.id}
-        style={options.styles.list}
-        ListHeaderComponent={searchView()}
-        onEndReachedThreshold={0.1}
-        onEndReached={fetchMoreData}
-        showsVerticalScrollIndicator={false}
-        ItemSeparatorComponent={separator}
-      />
-    </View>
-  );
+      <FlatList data={filterList} renderItem={_renderItem} keyExtractor={item => item.id} style={options.styles.list} ListHeaderComponent={searchView()} onEndReachedThreshold={0.1} onEndReached={fetchMoreData} showsVerticalScrollIndicator={false} ItemSeparatorComponent={separator} />
+    </View>;
 };
 
 export default {
@@ -114,3 +72,9 @@ export default {
   navigator: FAQ,
   slice
 };
+
+const _styles = StyleSheet.create({
+  HaVZuDPd: {
+    padding: 5
+  }
+});
--- modules/faq/options.js
+++ modules/faq/options.js
@@ -1,10 +1,7 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"; // to configure this module, edit the base url in options/options.js
 
-// to configure this module, edit the base url in options/options.js
 const path = "/modules/faq/";
-
 const title = "Support & FAQ";
-
 const recordsPerPage = 10; // by default per page return 10 FAQs, you can edit this as per your project requirement
 
 export const colors = {
@@ -16,15 +13,20 @@ export const colors = {
   darkGray: "#808080",
   black: "#000000"
 };
-
 const styles = StyleSheet.create({
-  container: { flex: 1, paddingHorizontal: 20 },
+  container: {
+    flex: 1,
+    paddingHorizontal: 20
+  },
   heading: {
     flexDirection: "row",
     justifyContent: "center",
     paddingVertical: 10
   },
-  backIcon: { width: 20, height: 20 },
+  backIcon: {
+    width: 20,
+    height: 20
+  },
   title: {
     fontSize: 16,
     flex: 1,
@@ -39,14 +41,19 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10
   },
-  searchIcon: { width: 25, height: 20, marginEnd: 5 },
+  searchIcon: {
+    width: 25,
+    height: 20,
+    marginEnd: 5
+  },
   input: {
     color: "#000000",
     fontSize: 16
   },
-  list: { marginTop: 10 }
+  list: {
+    marginTop: 10
+  }
 });
-
 export default {
   title: title,
   path: path,
