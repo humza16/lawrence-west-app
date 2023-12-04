ba7a190fc1599824fb3a8568df10761fa4ab87ae
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-12-04 22:15:35 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "lawrence-west-app-44469",
   url: "https://lawrence-west-app-44469.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
