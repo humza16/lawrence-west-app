b3364fabeb1500b34b745367d15614c4b049a8b1
Meer Humza <meer.humza@profitoptics.com>
2023-12-22 16:32:03 +0500

    project setup

---
+++ backend/web_build/static/js/994.32aa244e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_app=self.webpackChunktest_app||[]).push([[994],{994:(e,s,t)=>{t.r(s),t.d(s,{default:()=>p});t(791);var a=t(184);const p=()=>(0,a.jsx)(a.Fragment,{children:"Homepage"})}}]);
+//# sourceMappingURL=994.32aa244e.chunk.js.map
